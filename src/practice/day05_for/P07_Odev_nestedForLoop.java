2ccb6357661f5b071fdade3cba290f41f08384c8
saitguelen <saidgln25@gmail.com>
2025-06-07 14:07:00 +0200

    Added Practice Example-for-while-Loop and Array

---
+++ src/practice/day05_for/P07_Odev_nestedForLoop.java
@@ -0,0 +1,35 @@
+package practice.day05_for;
+
+public class P07_Odev_nestedForLoop {
+    /*
+     Yıldızlar (*) ile piramit oluşturabileceğiniz bir kod yazınız.
+            Sonuç böyle olmalıdır;
+
+                   *
+                  * *
+                 * * *
+                * * * *
+               * * * * *
+
+
+               şeklini konsola yazdiriniz.
+     */
+    public static void main(String[] args) {
+        // Piramit yüksekliğini belirle
+        int n = 5;
+
+        // Piramidi oluştur
+        for (int i = 1; i <= n; i++) {
+            // Boşlukları yazdır
+            for (int j = n - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            // Yıldızları yazdır
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            // Satır sonunu belirt
+            System.out.println();
+        }
+    }
+}
